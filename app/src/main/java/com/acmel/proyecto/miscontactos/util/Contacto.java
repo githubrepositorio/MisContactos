1b89bbe1424fc99d4864a586c85d5d3db524487f
Julio <juliowidmann@hotmail.com>
2016-03-05 20:20:07 -0500

    Actualizacion 5-03-2016 8.19 PM

---
+++ app/src/main/java/com/acmel/proyecto/miscontactos/util/Contacto.java
@@ -0,0 +1,47 @@
+package com.acmel.proyecto.miscontactos.util;
+
+/**
+ * Created by JULIOENRIQUE on 5/03/2016.
+ */
+public class Contacto {
+    private String nombre,telefono,email,direccion;
+
+    public Contacto(String nombre, String telefono, String email, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
