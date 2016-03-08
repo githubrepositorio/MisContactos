b6f9af82ed2ad0126e7af2b98ee4b7bb3d31788e
Julio <juliowidmann@hotmail.com>
2016-03-08 13:34:10 -0500

    Actualizacion 6-03-2016 10.40 AM

--- app/src/main/java/com/acmel/proyecto/miscontactos/MainActivity.java
+++ app/src/main/java/com/acmel/proyecto/miscontactos/MainActivity.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.Toast;
 
+import com.acmel.proyecto.miscontactos.util.Contacto;
 import com.acmel.proyecto.miscontactos.util.TextChangedListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtNombre, txtTelefono, txtEmail, txtDireccion;
+    private List<Contacto> contactos = new ArrayList<Contacto>();
+    private ListView contactsListView;
     private Button btnAgregar;
 
     @Override
@@ -19,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inicializarComponentesUI();
+        inicializarTabs();
+    }
+
+    private void inicializarTabs() {
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec = tabHost.newTabSpec("tab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Crear");
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("tab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Lista");
+        tabHost.addTab(spec);
     }
 
     private void inicializarComponentesUI() {
@@ -27,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         txtTelefono = (EditText)findViewById(R.id.cmpTelefono);
         txtEmail = (EditText) findViewById(R.id.cmpEmail);
         txtDireccion = (EditText) findViewById(R.id.cmpDireccion);
+        contactsListView = (ListView)findViewById(R.id.listView);
 
         btnAgregar = (Button) findViewById(R.id.btnAgregar);
         btnAgregar.setEnabled(false);
@@ -67,10 +92,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        agregarContacto(
+                txtNombre.getText().toString(),
+                txtTelefono.getText().toString(),
+                txtEmail.getText().toString(),
+                txtDireccion.getText().toString()
+        );
         String mesg = String.format("%s ha sido agregado a la lista!",txtNombre.getText());
         Toast.makeText(this, mesg, Toast.LENGTH_SHORT).show();
         btnAgregar.setEnabled(false);
+        inicializarListView();
         limpíarCampos();
+    }
+
+    private void inicializarListView() {
+
+    }
+
+    private void agregarContacto(String nombre, String telefono, String email, String direccion) {
+        contactos.add(new Contacto(nombre,telefono,email,direccion));
     }
 
     private void limpíarCampos() {
